9c196411b28156e40d2f5750fc06ade6bc4d101e
Mike Gasparelli <magasparel@gmail.com>
2016-08-16 21:33:00 -0400

    Add unix-like home alias '~' and cd-{n} command

--- Aliases.ps1
+++ Aliases.ps1
@@ -1,4 +1,5 @@
-﻿set-alias ex explorer
+set-alias ex explorer
 set-alias codi code-insiders
 set-alias touch Touch-File
 set-alias -Name cd -Value Set-LocationImproved -Option AllScope
+set-alias ~ $env:USERPROFILE
